5fab727a15504c66273c44eed77d76f47919c984
Gabriel Silva Jorge <gabriel.sjorge@inss.gov.br>
2026-02-23 22:01:19 -0300

    Add: ajustes na estrutura do código

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link'
 import { createClient } from '@/lib/supabase/client'
 import { useRouter } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+
 export default function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
